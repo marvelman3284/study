81bf1f00ea21a2ece05d4a026059ef85820cdbfb
Jeevan Shah <jedimaster3284@gmail.com>
2023-11-12 11:32:34 -0500

    save check before mongo migration

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,10 +1,20 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { flashcardList } from '../helpers/types';
+import axios from 'axios';
 
 export default function App() {
   const [formData, setFormData] = useState<flashcardList>([{ id: uuidv4(), term: '', def: '' }]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState<string>("");
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/cards')
+      .then(response => setCards(response.data))
+      .catch(error => console.error(error));
+    }, []);
 
   const handleInputChange = (id: string, input: string, value: string) => {
     const updatedFormData = formData.map((item) =>
@@ -25,30 +35,52 @@ export default function App() {
 
   const handleSubmit = () => {
     console.log("Submitted");
-    console.log(formData[0].term);
+    console.log(formData);
+    createDeck(title, 0, formData, description);
+  }
+
+  const handleTitleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  }
+
+  const handleDescChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(e.target.value);
   }
 
   return (
     <div>
-      {formData.map((item) => (
-        <div key={item.id}>
-          <input
-            type="text"
-            placeholder="Term"
-            value={item.term}
-            onChange={(e) => handleInputChange(item.id, 'term', e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Definition"
-            value={item.def}
-            onChange={(e) => handleInputChange(item.id, 'def', e.target.value)}
-          />
-          <button onClick={() => handleRemoveRow(item.id)}>Remove</button>
-        </div>
-      ))}
-      <button onClick={handleAddRow}>Add Row</button>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
+      <h1> Card List </h1>
+      <ul>
+        {cards.map(card => (
+          <li key={card.Flashcard_ID}>{card.FrontText}</li>
+        ))}
+      </ul>
     </div>
   );
+
+  // return (
+  //   <div>
+  //     <input type="text" placeholder="Deck Title" value={title} onChange={handleTitleChange}/>
+  //     <input type="text" placeholder="Description (optional)" value={description} onChange={handleDescChange}/>
+  //     {formData.map((item) => (
+  //       <div key={item.id}>
+  //         <input
+  //           type="text"
+  //           placeholder="Term"
+  //           value={item.term}
+  //           onChange={(e) => handleInputChange(item.id, 'term', e.target.value)}
+  //         />
+  //         <input
+  //           type="text"
+  //           placeholder="Definition"
+  //           value={item.def}
+  //           onChange={(e) => handleInputChange(item.id, 'def', e.target.value)}
+  //         />
+  //         <button onClick={() => handleRemoveRow(item.id)}>Remove</button>
+  //       </div>
+  //     ))}
+  //     <button onClick={handleAddRow}>Add Row</button>
+  //     <button type="submit" onClick={handleSubmit}>Submit</button>
+  //   </div>
+  // );
 };
