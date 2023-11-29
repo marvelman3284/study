254bc1e45c22bd015700ef3afe709a74ac915f8f
Jeevan Shah <jedimaster3284@gmail.com>
2023-11-28 19:03:02 -0500

    fixes #1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import {Nav} from './components/nav'
+import { Nav } from "./components/nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body>
         <Providers>
-        <Nav />
+          <Nav />
           {children}
         </Providers>
       </body>
